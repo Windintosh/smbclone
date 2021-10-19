e60928876456893561cc1120a26bba1354bcac5e
JaeHong Park @ Steelval-Port <48243825+Windintosh@users.noreply.github.com>
2021-10-19 18:42:40 +0900

    upload other modules to use

--- smbgamedemo.py
+++ smbgamedemo.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import game_framework
 import random
 
 
---
+++ start_state.py
@@ -0,0 +1,57 @@
+import game_framework
+from pico2d import *
+import title_state
+
+
+name = "StartState"
+image = None
+logo_time = 0.0
+
+
+def enter():
+    global image
+    image = load_image('assets/kpu_credit.png')
+    pass
+
+
+def exit():
+    global image
+    del image
+    pass
+
+
+def update():
+    global logo_time
+
+    if logo_time > 1.0 :
+        logo_time = 0
+        # game_framework.quit()
+        game_framework.change_state(title_state)
+    delay(0.01)
+    logo_time += 0.01
+    pass
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+    pass
+
+
+
+
+def handle_events():
+    events = get_events()
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
+
+
+
+
